d36fd37d55b968c7e5fafcb957115fa165270fe2
otinu <fitshter@gmail.com>
2021-09-24 20:07:24 +0900

    FirstCommit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 福田哲也
+ *
+ */
+module sukkiri_Java_beginner {
+}
---
+++ src/sukkiri_Java_beginner/test.java
@@ -0,0 +1,10 @@
+package sukkiri_Java_beginner;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
