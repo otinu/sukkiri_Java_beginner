d6c44733a6a49139247f50f3ea516bfc5f17ef7f
otinu <fitshter@gmail.com>
2021-09-25 19:01:56 +0900

    [make]CharStringSample

---
+++ src/sukkiri_Java_beginner/CharStringSample.java
@@ -0,0 +1,20 @@
+package sukkiri_Java_beginner;
+
+public class CharStringSample {
+
+	public static void main(String[] args) {
+		String st = "1";
+		System.out.println(st);
+
+		char ch = '1';
+		System.out.println(ch);
+
+		/*
+		// 「"」は文字列型として扱われ、「'」は文字型として扱われる
+
+		st = '2'; //どちらもEclipseで代入できない旨を警告してくれる
+		ch="3";
+		*/
+	}
+
+}
