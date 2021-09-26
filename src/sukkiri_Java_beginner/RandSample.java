7c5242033d49cde65a9339561b4df0486e770123
otinu <fitshter@gmail.com>
2021-09-26 16:21:39 +0900

    [make]RandSample

---
+++ src/sukkiri_Java_beginner/RandSample.java
@@ -0,0 +1,11 @@
+package sukkiri_Java_beginner;
+
+public class RandSample {
+
+	public static void main(String[] args) {
+		int r = new java.util.Random().nextInt(90); //乱数生成
+		System.out.println(r);
+
+	}
+
+}
