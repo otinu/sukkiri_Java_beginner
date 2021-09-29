2cd30cc5dd730ef13d3a135a2e670a1b1523a784
otinu <fitshter@gmail.com>
2021-09-29 10:13:34 +0900

    [make]ImportSamle

---
+++ src/sukkiri_Java_beginner/Calc.java
@@ -0,0 +1,14 @@
+package sukkiri_Java_beginner;
+
+import sukkiri_Java_beginner.CalcLogic; //パッケージ名.クラス名 でインポート
+
+public class Calc {
+
+	public static void main(String[] args) {
+		int a = 10; int b = 2;
+	    int total = CalcLogic.tasu(a, b);
+	    int delta = CalcLogic.hiku(a, b);
+	    System.out.println("足すと" + total + "、引くと" + delta);
+	}
+
+}
---
+++ src/sukkiri_Java_beginner/CalcLogic.java
@@ -0,0 +1,12 @@
+package sukkiri_Java_beginner;
+
+public class CalcLogic {	//呼び出される側はmainで囲わない
+
+	  public static int tasu(int a, int b) {
+	    return (a + b);
+	  }
+
+	  public static int hiku(int a, int b) {
+	    return (a - b);
+	  }
+}
