1a6e36f4ef587adcad3dee441c2733d48f29c916
otinu <fitshter@gmail.com>
2021-09-30 16:59:25 +0900

    [make]VarSample

---
+++ src/sukkiri_Java_beginner/VarSample.java
@@ -0,0 +1,10 @@
+package sukkiri_Java_beginner;
+
+import java.util.HashMap;
+
+public class VarSample {
+
+	HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+	var mapping = new HashMap<String, Integer>();
+}
